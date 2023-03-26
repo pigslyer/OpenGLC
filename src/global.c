22a4f927f3551203e3dc5980308860a5425a8e99
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-03-26 07:40:00 +0200

    it makes no sense to keep viewport and screen coordinates separate in core, at least for the moment

--- src/global.c
+++ src/global.c
@@ -4,8 +4,8 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-const int VIEWPORT_WIDTH = 800;
-const int VIEWPORT_HEIGHT = 600;
+const int VIEWPORT_WIDTH = SCREEN_WIDTH;
+const int VIEWPORT_HEIGHT = SCREEN_HEIGHT;
 
 const int RENDER_RAYCAST_COUNT = 120;
 const int RENDER_RAYCAST_DOF_MAX = 8;
