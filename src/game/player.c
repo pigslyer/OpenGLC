2bd323da4dbf70de0da5e1f1bcefeb5d16c98d04
pigslyer <63311292+pigslyer@users.noreply.github.com>
2023-04-07 10:46:07 +0200

    added WIP player collision

--- src/game/player.c
+++ src/game/player.c
@@ -3,6 +3,7 @@
 
 #define PLAYER_MOVEMENT_SPEED 5
 #define PLAYER_TURN_SPEED 0.08f
+#define PLAYER_COLLISION_LOOKAHEAD 5
 
 #define PLAYER_BEAK_LENGTH 40
 
@@ -15,6 +16,8 @@ void playerInit(void)
 	playerRotation = PIf;
 }
 
+#define MAX_SLIDES 8
+
 void playerUpdate(void)
 {
 	float turning = (float)(GET_KEY(GLFW_KEY_A) - GET_KEY(GLFW_KEY_D));
@@ -25,8 +28,41 @@ void playerUpdate(void)
 	float sin, cos;
 	sincosf(playerRotation, &sin, &cos);
 
-	playerPosition.x += forward * cos * PLAYER_MOVEMENT_SPEED;
-	playerPosition.y -= forward * sin * PLAYER_MOVEMENT_SPEED;
+	rayResults collisionResult;
+	rayData* finalResult;
+	vec2f speed = VEC2F2_1(forward * cos * PLAYER_MOVEMENT_SPEED, forward * sin * PLAYER_MOVEMENT_SPEED);
+
+	// move and slide implementation
+	for (int slideCount = 0; slideCount < MAX_SLIDES && (speed.x != 0 || speed.y != 0); slideCount++)
+	{
+		castRayV(playerPosition, speed, &collisionResult);
+
+		// there's absolutely nothing in our way
+		if (collisionResult.resultCount == 0)
+		{
+			playerPosition.x += speed.x;
+			playerPosition.y -= speed.y;
+			break;
+		}
+
+		finalResult = &(collisionResult.results[collisionResult.resultCount - 1]);
+
+		if (SQR(finalResult->rayLength) > lenV2fSqr(speed) || true)
+		{
+			playerPosition.x += speed.x;
+			playerPosition.y -= speed.y;
+			break;
+		}
+
+		printf("detected a collision\n");
+		// first configure new speed since we need the previous player position to figure out "actual" speed delta
+		speed.x = (collisionResult.normal.x == 0) * (speed.x - (finalResult->globalPosHit.x - playerPosition.x));
+		speed.y = (collisionResult.normal.y == 0) * (speed.y - (finalResult->globalPosHit.y - playerPosition.y));
+
+		// move to wall collision point, pushed away from the wall by collision lookahead (so we can't rub out face on the wall)
+		playerPosition.x = finalResult->globalPosHit.x + collisionResult.normal.x * PLAYER_COLLISION_LOOKAHEAD;
+		playerPosition.y = finalResult->globalPosHit.y + collisionResult.normal.y * PLAYER_COLLISION_LOOKAHEAD;
+	}
 }
 
 void playerDraw(void)
